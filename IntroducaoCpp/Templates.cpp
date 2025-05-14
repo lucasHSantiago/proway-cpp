43b1cfc6cb288c02f3937def6626135932e3e162
lucashsantiago <lucashenriquesantiago@gmail.com>
2025-05-14 14:31:22 -0300

    exercício templates.

--- IntroducaoCpp/Templates.cpp
+++ IntroducaoCpp/Templates.cpp
@@ -44,4 +44,40 @@ namespace Templates
             return 0;
         }
     }
+
+    namespace Exercicio
+    {
+        /*
+        1. Criar uma função genérica de max (maior valor)
+        2. Criar para int, double, float e string
+        */
+
+        template<typename T>
+        T Max(T a, T b)
+        {
+            if (a > b)
+                return a;
+
+            return b;
+        }
+
+        template<>
+        const std::string& Max(const std::string& a, const std::string& b)
+        {
+            if (a.length() > b.length())
+                return a;
+
+            return b;
+        }
+
+        int main()
+        {
+            int i = Max(1, 2);
+            double d = Max(1.0, 2.0);
+            float f = Max(1.0f, 2.0f);
+            std::string s = Max("abc", "a");
+
+            return 0;
+        }
+    }
 }
