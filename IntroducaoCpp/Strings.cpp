bbf4e28e57d7b77544cddba429f1669d162523ce
lucashsantiago <lucashenriquesantiago@gmail.com>
2025-05-14 14:34:32 -0300

    exercício strings

--- IntroducaoCpp/Strings.cpp
+++ IntroducaoCpp/Strings.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -14,5 +15,39 @@ namespace Strings
         string primeiroNome = nome.substr(0, 5);
 
         return 0;
+    }
+
+    namespace Exercicio
+    {
+        /*
+            1. Peça ao usuário para digitar duas frases.
+            2. Mostre o tamanho (número de caracteres) de cada frase.
+            3. Verifique se as frases são iguais.
+        */
+        int main()
+        {
+            std::string frase1, frase2;
+
+            std::cout << "Digite a primeira frase: ";
+            std::getline(std::cin, frase1);
+
+            std::cout << "Digite a segunda frase: ";
+            std::getline(std::cin, frase2);
+
+            std::cout << "\nTamanho da primeira frase: " << frase1.length() << " caracteres\n";
+            std::cout << "Tamanho da segunda frase: " << frase2.length() << " caracteres\n";
+
+            if (frase1 == frase2)
+            {
+                std::cout << "As frases sao iguais.\n";
+
+            }
+            else
+            {
+                std::cout << "As frases sao diferentes.\n";
+            }
+
+            return 0;
+        }
     }
 }
